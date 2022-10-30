2aa56435a72cd3c5dfaecf1c39e8cc1751f48c91
Alex <al-an2017@rambler.ru>
2022-10-31 00:26:04 +0300

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа, которая принимает на вход число и выдает является ли оно четным
+Console.WriteLine("Введите число");
+
+int num = Convert.ToInt32(Console.ReadLine());
+
+int result = num%2;
+
+if(result==0){
+    Console.WriteLine("Число четное");
+}
+else{
+   Console.WriteLine("Число нечетное"); 
+}
