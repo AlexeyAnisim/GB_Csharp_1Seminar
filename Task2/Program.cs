5fb125423ce639aecb6df773dd675d9d2406f18c
Alex <al-an2017@rambler.ru>
2022-10-30 23:36:12 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,13 @@
+﻿// Программа, которая принимает на вход три числа и выдает максимальное из них
+Console.WriteLine("Введите три разных числа");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int max = num1;
+
+if(num2>max) max = num2;
+if(num3>max) max = num3;
+{
+    Console.WriteLine($"Максимальное число - {max}");
+}
