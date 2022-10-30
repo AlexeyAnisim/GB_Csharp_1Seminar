a7c4e17c92e0d627ddd252076ab626cb41e3bb2a
Alex <al-an2017@rambler.ru>
2022-10-30 21:48:56 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿// На вход принимает 2 числа и выдает большее и меньшее
+Console.WriteLine("Введите два любых числа");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+if (num1>num2){
+    Console.WriteLine($"Большее число: {num1}, Меньшее число: {num2}");
+}
+else
+{
+   Console.WriteLine($"Большее число: {num2}, Меньшее число: {num1}"); 
+}
