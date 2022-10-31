056c58a1585d38930cebc9cc712a19fa6e3d1c75
Alex <al-an2017@rambler.ru>
2022-10-31 17:01:16 +0300

    Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Программа вывода четных чисел от 1 до N
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+for(int num1 = 1; num1<=num; num1 ++)
+{
+    if(num1%2 == 0)
+    {
+        Console.WriteLine(num1 + " ");
+    }
+}
+if(num == 1)
+{
+    Console.WriteLine("нет четных чисел");
+}
